4c32852ac474d8841bc85c6e6f88bb00a9c0a6ae
Ashutosh876 <ashutosha876@gmail.com>
2022-09-03 01:43:46 +0530

    initial commit

---
+++ src/main/java/com/realworld/realworldbackend/RealworldBackendApplication.java
@@ -0,0 +1,13 @@
+package com.realworld.realworldbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealworldBackendApplication.class, args);
+	}
+
+}
